bb230f5325ad3a9eddf1696cdc14064484e2377f
ZooHairDev <46304063+ZooHairDev@users.noreply.github.com>
2023-11-26 15:44:22 +0100

    Build sort selector

--- src/assets/services/image-url.ts
+++ src/assets/services/image-url.ts
@@ -1,5 +1,9 @@
 
+import noImage from "../No-Image-Placeholder.png"
+
 const getCroppedImgUrl = (url: string) => {
+    if (!url) return noImage;
+
     const target = "media/";
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + "crop/600/400/" + url.slice(index);
